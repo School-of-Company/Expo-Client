e1f1dcd0664c4c0f92758690d68cbe56c0efa559
Ethen1264 <jxorhks86909763@gmail.com>
2024-10-28 16:37:26 +0900

    🐛Fix: header height bug

--- src/widgets/layout/ui/Header/index.tsx
+++ src/widgets/layout/ui/Header/index.tsx
@@ -3,7 +3,7 @@ import { Logo, Navigation } from '@/entities/layout/Header';
 
 const Header = () => {
   return (
-    <div className="mx-auto flex h-[90px] w-full">
+    <div className="mx-auto flex w-full py-5">
       <div className="mx-auto flex w-[1200px] items-center justify-between mobile:px-5">
         <Logo />
         <Navigation />
