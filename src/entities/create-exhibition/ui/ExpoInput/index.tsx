718a20c846d22137f76a20b0efb477e61dff19cd
Ethen1264 <jxorhks86909763@gmail.com>
2024-10-31 23:27:12 +0900

    🎨Update: object gap design

--- src/entities/create-exhibition/ui/ExpoInput/index.tsx
+++ src/entities/create-exhibition/ui/ExpoInput/index.tsx
@@ -10,34 +10,31 @@ interface Props {
 
 const ExpoInput = ({ onChangeInput, inputs, addInput, removeInput }: Props) => {
   return (
-    <div>
-      <div className="flex flex-col gap-4">
-        {inputs.map((input, index) => (
-          <div key={index} className="flex items-center justify-between">
-            <div className="flex flex-grow items-center gap-6">
-              <p className="flex-shrink-0 text-body4 text-gray-500">
-                {index + 1}
-              </p>
-              <input
-                placeholder="연수를 입력해주세요"
-                className="w-full bg-transparent text-body4 text-gray-500"
-                value={input}
-                onChange={(e) => onChangeInput(index, e.target.value)}
-              />
+    <div className="flex flex-col gap-[20px]">
+      {inputs.length > 0 && (
+        <div className="flex flex-col gap-4">
+          {inputs.map((input, index) => (
+            <div key={index} className="flex items-center justify-between">
+              <div className="flex flex-grow items-center gap-6">
+                <p className="text-body4 text-gray-500">{index + 1}</p>
+                <input
+                  placeholder="연수를 입력해주세요"
+                  className="w-full bg-transparent text-body4 text-gray-500"
+                  value={input}
+                  onChange={(e) => onChangeInput(index, e.target.value)}
+                />
+              </div>
+              <button
+                onClick={() => removeInput(index)}
+                className="hover:cursor-pointer"
+              >
+                <XMark />
+              </button>
             </div>
-            <button
-              onClick={() => removeInput(index)}
-              className="flex-shrink-0 hover:cursor-pointer"
-            >
-              <XMark />
-            </button>
-          </div>
-        ))}
-      </div>
-      <button
-        className="mx-auto mt-6 flex items-center gap-5"
-        onClick={addInput}
-      >
+          ))}
+        </div>
+      )}
+      <button className="mx-auto flex items-center gap-5" onClick={addInput}>
         <Plus fill="#448FFF" />
         <div className="text-body3 text-main-600">추가하기</div>
       </button>
