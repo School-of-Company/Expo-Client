66818e72979e409966a92d6e8319a45da94ae765
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-02 12:22:04 +0900

    🎨Add ContentText components

---
+++ src/entities/expo-detail/ui/ContentText/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+  title: string;
+  content: string;
+}
+
+const ContentText = ({ title, content }: Props) => {
+  return (
+    <div className="space-y-4">
+      <p className="text-body1 text-gray-600">{title}</p>
+      <p className="text-body2 text-gray-400">{content}</p>
+    </div>
+  );
+};
+
+export default ContentText;
