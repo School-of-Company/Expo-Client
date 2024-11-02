d9dde2100c720d21070faf944707da046e5495b4
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-02 11:43:25 +0900

    ♻️ Refactor data processing module

--- src/shared/ui/Button/index.tsx
+++ src/shared/ui/Button/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Button = ({ text, onClick }: Props) => {
   return (
     <button
-      onClick={onClick}
+      onClick={onClick || (() => {})}
       className="w-full rounded-sm bg-main-600 py-4 text-body1 text-white"
     >
       {text}
