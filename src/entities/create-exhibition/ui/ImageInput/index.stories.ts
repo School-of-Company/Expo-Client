ca84b239d71af72525d64e4461dee69e8b6b728f
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-02 11:57:30 +0900

    ✨Add: storybook img setting

--- src/entities/create-exhibition/ui/ImageInput/index.stories.ts
+++ src/entities/create-exhibition/ui/ImageInput/index.stories.ts
@@ -11,5 +11,15 @@ export default meta;
 type Story = StoryObj<typeof ImageInput>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    img: null,
+    setImg: () => {},
+  },
+};
+
+export const WithImage: Story = {
+  args: {
+    img: 'https://cdn.discordapp.com/attachments/857824085509799936/1302103898215153764/20240626_192254.jpg?ex=6726e641&is=672594c1&hm=c653865724d6696c043fe6e37ab5cb247c7ba4b374a8520feb0645d2eab55e2f&',
+    setImg: () => {},
+  },
 };
