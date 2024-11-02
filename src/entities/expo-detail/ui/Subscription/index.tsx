61a56a64f4bc5122733d4e05d5741af948b148e8
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-03 00:50:25 +0900

    🎨Add: Subscription components

---
+++ src/entities/expo-detail/ui/Subscription/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@/shared/ui';
+
+const Subscription = () => {
+  return (
+    <div className="space-y-[26px] rounded-sm border-1 border-solid border-gray-200 p-[18px]">
+      <p className="text-blak text-caption1">2024 AI광주미래교육박람회</p>
+      <Button text="신청하기" />
+    </div>
+  );
+};
+
+export default Subscription;
