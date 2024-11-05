f9b69fe7ef06c6ec1b2402982c4c09c23618c864
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-05 17:01:09 +0900

    🎨Add expo-manage page

--- src/views/expo-manage/ui/expo-manage/index.tsx
+++ src/views/expo-manage/ui/expo-manage/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FilterTab, TableForm } from '@/widgets/expo-manage';
+import { Header } from '@/widgets/layout';
 
 const ExpoManage = () => {
   return (
-    <div>
-      <FilterTab />
-      <TableForm />
+    <div className="flex h-screen flex-col gap-[30px] mobile:gap-5">
+      <Header />
+      <div className="mx-auto w-full max-w-[1200px] space-y-[30px] px-5">
+        <FilterTab />
+        <TableForm />
+      </div>
     </div>
   );
 };
--- src/widgets/expo-manage/ui/TableForm/index.tsx
+++ src/widgets/expo-manage/ui/TableForm/index.tsx
@@ -7,7 +7,14 @@ const TableForm = () => {
     <div className="space-y-[34px] rounded-sm border-1 border-solid border-gray-200 px-[30px] py-6">
       <div className="space-y-[30px] border-b-1 border-solid border-gray-100 pb-6">
         <TableHeader />
-        <div className="space-y-6">
+        <div className="max-h-[498px] space-y-6 overflow-y-auto [&::-webkit-scrollbar-thumb]:rounded-md [&::-webkit-scrollbar-thumb]:bg-main-400 [&::-webkit-scrollbar-track]:rounded-md [&::-webkit-scrollbar-track]:bg-main-100 [&::-webkit-scrollbar]:w-2">
+          <TableItem />
+          <TableItem />
+          <TableItem />
+          <TableItem />
+          <TableItem />
+          <TableItem />
+          <TableItem />
           <TableItem />
           <TableItem />
           <TableItem />
