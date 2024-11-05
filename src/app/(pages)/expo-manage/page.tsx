e17a4ed72ed540dee112e22be5e108f315c53dda
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-05 12:06:50 +0900

    🎨Add Set up TableForm component layout structure

---
+++ src/app/(pages)/expo-manage/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableForm from '@/widgets/expo-manage/ui/TableForm';
+
+const page = () => {
+  return (
+    <div>
+      <TableForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/widgets/expo-manage/ui/TableForm/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TableForm = () => {
+  return (
+    <div className="space-y-[34px] rounded-sm border-1 border-solid border-gray-200 px-[30px] py-6">
+      <div className="space-y-[30px] border-b-1 border-solid border-gray-100 pb-6">
+        <div className="bg-red-400">테이블 헤더</div>
+        <div className="space-y-6">
+          <div className="bg-blue-400">테이블 아이템</div>
+          <div className="bg-blue-400">테이블 아이템</div>
+          <div className="bg-blue-400">테이블 아이템</div>
+          <div className="bg-blue-400">테이블 아이템</div>
+        </div>
+      </div>
+      <div className="bg-yellow-300 px-6">테이블 푸터</div>
+    </div>
+  );
+};
+
+export default TableForm;
