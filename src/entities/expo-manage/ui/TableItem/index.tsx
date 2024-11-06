cedde44ae04c4f7d4fb07f383ede8f3e2444ed6b
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-06 09:04:54 +0900

    💄Update: Header navigation

--- src/entities/expo-manage/ui/TableItem/index.tsx
+++ src/entities/expo-manage/ui/TableItem/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TableItem = () => {
   const items = ['1', '김진원', '고등학교', '학생', '수학', '01012345678'];
   return (
-    <div className="flex items-center justify-between py-2">
+    <div className="mr-5 flex items-center justify-between rounded-sm border-1 border-solid border-gray-200 py-2">
       {items.map((item, index) => (
         <div key={index} className="flex-1">
           <p className="text-center text-caption1 text-gray-500">{item}</p>
