eb08b18f3b715fa79dcc836e867c8d63570f9dfc
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-15 01:02:47 +0900

    ✨Add: expo list api

--- src/entities/main/ui/ExpoListItem/index.tsx
+++ src/entities/main/ui/ExpoListItem/index.tsx
@@ -1,48 +1,74 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
-import TestImg from '@/shared/assets/png/TestImg.png';
 
 interface Props {
-  clubName: string;
+  id: number;
+  coverImage: string;
+  title: string;
   description: string;
-  startDate: string;
-  endDate: string;
+  startedDay: string;
+  finishedDay: string;
 }
 
-const ExpoListItem = ({ clubName, description, startDate, endDate }: Props) => {
+const ExpoListItem = ({
+  id,
+  coverImage,
+  title,
+  description,
+  startedDay,
+  finishedDay,
+}: Props) => {
   return (
-    <div className="flex gap-6 rounded-md border-1 border-solid border-gray-200 bg-white px-6 py-5">
-      <div className="relative overflow-hidden">
+    <Link
+      href={`/expo-detail/${id}`}
+      className="flex gap-6 rounded-md border-1 border-solid border-gray-200 bg-white px-6 py-5"
+    >
+      <div className="w-1/3">
         <Image
-          src={TestImg}
-          alt="TestClubImg"
-          objectFit="cover"
-          className="rounded-md"
+          src={coverImage}
+          alt="이미지 설명"
+          width={752}
+          height={360}
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            objectPosition: 'center',
+          }}
         />
       </div>
-      <div className="flex-1">
+      <div className="w-2/3">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col gap-1">
-            <p className="text-caption2 text-gray-600">
-              모집기간: {startDate} ~ {endDate}
-            </p>
             <p
-              className="truncate text-body1 text-black"
+              className="overflow-hidden text-caption2 text-gray-600"
               style={{
-                whiteSpace: 'nowrap',
-                overflow: 'hidden',
+                display: '-webkit-box',
+                WebkitLineClamp: 1,
+                WebkitBoxOrient: 'vertical',
                 textOverflow: 'ellipsis',
               }}
             >
-              {clubName}
+              모집기간: {startedDay} ~ {finishedDay}
+            </p>
+            <p
+              className="overflow-hidden text-body1 text-black"
+              style={{
+                display: '-webkit-box',
+                WebkitLineClamp: 1,
+                WebkitBoxOrient: 'vertical',
+                textOverflow: 'ellipsis',
+              }}
+            >
+              {title}
             </p>
             <p
               className="overflow-hidden text-body4 text-gray-300"
               style={{
                 display: '-webkit-box',
-                WebkitLineClamp: 2,
+                WebkitLineClamp: 1,
                 WebkitBoxOrient: 'vertical',
-                overflow: 'hidden',
                 textOverflow: 'ellipsis',
               }}
             >
@@ -51,7 +77,7 @@ const ExpoListItem = ({ clubName, description, startDate, endDate }: Props) => {
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/widgets/main/ui/ExpoListContainer/index.tsx
+++ src/widgets/main/ui/ExpoListContainer/index.tsx
@@ -1,8 +1,32 @@
-import { ExpoListItem, Filter } from '@/entities/main';
+'use client';
 
-import { ExpoListMock } from '../../model/ExpoListMock';
+import { useEffect, useState } from 'react';
+import { ExpoListItem, Filter } from '@/entities/main';
+import { apiClient } from '@/shared/libs/apiClient';
+
+interface ExpoItem {
+  id: number;
+  coverImage: string;
+  title: string;
+  description: string;
+  startedDay: string;
+  finishedDay: string;
+}
 
 const ExpoListContainer = () => {
+  const [expoList, setExpoList] = useState<ExpoItem[]>([]);
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    apiClient
+      .get('/expo', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((res) => {
+        setExpoList(res.data);
+      });
+  }, []);
   return (
     <div>
       <div className="mb-[30px] flex justify-between">
@@ -11,13 +35,15 @@ const ExpoListContainer = () => {
       </div>
 
       <div className="grid grid-cols-3 gap-4 mobile:grid-cols-1">
-        {ExpoListMock.map((item, index) => (
+        {expoList.slice(0, 5).map((item, index) => (
           <ExpoListItem
+            id={item.id}
+            coverImage={item.coverImage}
             key={index}
-            clubName={item.clubName}
+            title={item.title}
             description={item.description}
-            startDate={item.startDate}
-            endDate={item.endDate}
+            startedDay={item.startedDay}
+            finishedDay={item.finishedDay}
           />
         ))}
       </div>
