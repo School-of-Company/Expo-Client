4a2b76f8921c8c0c61979c2fb8cfbe19a9b45458
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-18 23:35:07 +0900

    ✨Add: footerAction

---
+++ src/shared/model/footerActions.ts
@@ -0,0 +1,12 @@
+export const fileActions = {
+  exportPDF: () => console.log('PDF 내보내기'),
+  exportExcel: () => console.log('Excel 내보내기'),
+};
+
+export const printActions = {
+  printBadge: () => console.log('명찰 출력'),
+};
+
+export const deleteActions = {
+  DeleteBadge: () => console.log('명찰 출력'),
+};
