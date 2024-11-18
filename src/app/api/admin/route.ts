a921b54030530fe2379b99163e5cbb228110ca1d
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-18 23:43:15 +0900

    ✨Add: check signup user api

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -24,3 +24,35 @@ export async function GET() {
     return NextResponse.json({ error: message }, { status });
   }
 }
+
+export async function PATCH(request: Request) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+
+  const url = new URL(request.url);
+  const adminId = url.pathname.split('/').pop();
+
+  if (!adminId) {
+    return NextResponse.json(
+      { error: 'admin_id is required' },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const response = await apiClient.patch(`/admin/${adminId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message: string }>;
+
+    const status = axiosError.response?.status || 500;
+    const message = axiosError.response?.data?.message || 'admin failed';
+
+    return NextResponse.json({ error: message }, { status });
+  }
+}
