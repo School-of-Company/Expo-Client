b9a72f3f924f1abd45589e39a76421c4013fecb5
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-19 17:26:37 +0900

    ✨Add admin signup check api

--- src/app/api/admin/[admin_id]/route.ts
+++ src/app/api/admin/[admin_id]/route.ts
@@ -1,25 +1,30 @@
 import { AxiosError } from 'axios';
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { apiClient } from '@/shared/libs/apiClient';
 
-export async function GET() {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { admin_id: number } },
+) {
+  const { admin_id } = params;
   const cookieStore = cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
+  console.log(accessToken);
 
   try {
-    const response = await apiClient.get('/admin', {
+    const response = await apiClient.patch(`/admin/${admin_id}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
+
     return NextResponse.json(response.data);
   } catch (error) {
     const axiosError = error as AxiosError<{ message: string }>;
 
     const status = axiosError.response?.status || 500;
-    const message =
-      axiosError.response?.data?.message || 'request signup failed';
+    const message = axiosError.response?.data?.message || 'signup check failed';
 
     return NextResponse.json({ error: message }, { status });
   }
