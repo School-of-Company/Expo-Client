36789e7b37dd6358b25b19e575a92af0b504704a
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-20 17:11:08 +0900

    ♻️Refector: kakao map address api -> route api

--- src/widgets/create-exhibition/api/kakaoApi.ts
+++ src/widgets/create-exhibition/api/kakaoApi.ts
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-
 export const fetchAddressData = async (address: string) => {
-  const url = `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURIComponent(address)}`;
-
-  const response = await axios.get(url, {
-    headers: {
-      Authorization: `KakaoAK ${KAKAO_REST_API_KEY}`,
-    },
-  });
-
-  return response.data;
+  try {
+    const response = await axios.get(
+      `/api/map?address=${encodeURIComponent(address)}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching address data:', error);
+    throw error;
+  }
 };
