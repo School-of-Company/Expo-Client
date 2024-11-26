59b2a8e27102ccfab0075e5e447f1ec572f41941
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-26 14:49:55 +0900

    💚Fix: cicd

--- src/entities/create-exhibition/ui/TrainingModule/index.tsx
+++ src/entities/create-exhibition/ui/TrainingModule/index.tsx
@@ -9,9 +9,21 @@ import { ExhibitionFormData } from '@/widgets/create-exhibition/types/type';
 import ExpoInput from '../ExpoInput';
 
 interface Props {
-  fields: UseFieldArrayReturn<ExhibitionFormData, 'trainings', 'id'>['fields'];
-  append: UseFieldArrayReturn<ExhibitionFormData, 'trainings', 'id'>['append'];
-  remove: UseFieldArrayReturn<ExhibitionFormData, 'trainings', 'id'>['remove'];
+  fields: UseFieldArrayReturn<
+    ExhibitionFormData,
+    'trainings' | 'standard',
+    'id'
+  >['fields'];
+  append: UseFieldArrayReturn<
+    ExhibitionFormData,
+    'trainings' | 'standard',
+    'id'
+  >['append'];
+  remove: UseFieldArrayReturn<
+    ExhibitionFormData,
+    'trainings' | 'standard',
+    'id'
+  >['remove'];
   register: UseFormRegister<ExhibitionFormData>;
   setValue: UseFormSetValue<ExhibitionFormData>;
   watch: UseFormWatch<ExhibitionFormData>;
@@ -27,12 +39,19 @@ const TrainingModule = ({
   watch,
   fieldName,
 }: Props) => {
+  const createEmptyField = () => {
+    if (fieldName === 'trainings') {
+      return { title: '', startedAt: '', endedAt: '', category: 'CHOICE' };
+    }
+    return { title: '', startedAt: '', endedAt: '' };
+  };
+
   return (
     <div className="w-full rounded-sm border-1 border-solid border-gray-200 px-[30px] py-[26px]">
       <div className="w-full">
         <ExpoInput
           fields={fields}
-          append={append}
+          append={() => append(createEmptyField())}
           remove={remove}
           register={register}
           setValue={setValue}
