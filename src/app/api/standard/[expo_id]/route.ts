5ace89ecf00bd184cb7850631e6c2c51a86347ec
Ethen1264 <jxorhks86909763@gmail.com>
2024-12-02 00:07:50 +0900

    ✨Add: standard get route api

---
+++ src/app/api/standard/[expo_id]/route.ts
@@ -0,0 +1,37 @@
+import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { apiClient } from '@/shared/libs/apiClient';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { expo_id: number } },
+) {
+  const { expo_id } = params;
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  const config = accessToken
+    ? {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    : {};
+
+  try {
+    const response = await apiClient.get(`/standard/${expo_id}`, config);
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      const status = error.response?.status;
+      const message = error.response?.data?.message || 'Unknown error';
+      return NextResponse.json({ error: message }, { status: status || 500 });
+    } else {
+      return NextResponse.json(
+        { error: 'Internal Server Error' },
+        { status: 500 },
+      );
+    }
+  }
+}
