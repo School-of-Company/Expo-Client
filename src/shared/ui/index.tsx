9eba9a33695c236999435a076986a27bea732699
Ethen1264 <jxorhks86909763@gmail.com>
2024-12-10 00:14:05 +0900

    ♻️Refector: next js 14 loading -> HOC loading

--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -3,3 +3,4 @@ export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as PageHeader } from './PageHeader';
 export { default as CheckBox } from './CheckBox';
+export { default as Loading } from './Loading';
--- src/widgets/main/ui/ExpoListContainer/index.tsx
+++ src/widgets/main/ui/ExpoListContainer/index.tsx
@@ -1,33 +1,36 @@
 'use client';
 
 import { ExpoListItem, Filter } from '@/entities/main';
+import withLoading from '@/shared/hocs/withLoading';
 import { useExpoList } from '../../api/useExpoList';
 
 const ExpoListContainer = () => {
   const { data: expoList, isLoading } = useExpoList();
-  if (isLoading) return <div>Loading...</div>;
-  return (
-    <div>
-      <div className="mb-[30px] flex justify-between">
-        <p className="text-h1 text-black">박람회 신청</p>
-        <Filter />
-      </div>
 
-      <div className="grid grid-cols-3 gap-4 mobile:grid-cols-1">
-        {expoList?.map((item, index) => (
-          <ExpoListItem
-            id={item.id}
-            coverImage={item.coverImage}
-            key={index}
-            title={item.title}
-            description={item.description}
-            startedDay={item.startedDay}
-            finishedDay={item.finishedDay}
-          />
-        ))}
+  return withLoading({
+    isLoading,
+    children: (
+      <div>
+        <div className="mb-[30px] flex justify-between">
+          <p className="text-h1 text-black">박람회 신청</p>
+          <Filter />
+        </div>
+
+        <div className="grid grid-cols-3 gap-4 mobile:grid-cols-1">
+          {expoList?.map((item, index) => (
+            <ExpoListItem
+              id={item.id}
+              coverImage={item.coverImage}
+              key={index}
+              title={item.title}
+              description={item.description}
+              startedDay={item.startedDay}
+              finishedDay={item.finishedDay}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    ),
+  });
 };
-
 export default ExpoListContainer;
