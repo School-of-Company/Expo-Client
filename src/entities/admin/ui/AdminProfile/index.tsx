2c2c68e0ec50dbc8fcdef00aaef5e4390764145f
Ethen1264 <jxorhks86909763@gmail.com>
2025-01-15 16:09:18 +0900

    🎨Add: select fuction components

--- src/entities/admin/ui/AdminProfile/index.tsx
+++ src/entities/admin/ui/AdminProfile/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Logout } from '@/shared/assets/icons';
 import { useLogout } from '../../model/useLogout';
 
@@ -11,8 +11,14 @@ const ProfileInfo = ({ label, value }: { label: string; value: string }) => (
 
 const AdminProfile = () => {
   const { mutate: logout } = useLogout();
+  const [isToggleLogout, setIsToggleLogout] = useState(false);
+
+  const handleLogoutClick = () => {
+    setIsToggleLogout((prev) => !prev);
+  };
+
   return (
-    <div className="flex w-full justify-between">
+    <div className="relative flex w-full justify-between">
       <div className="flex items-center gap-[124px] mobile:flex-col mobile:gap-[30px]">
         <div className="space-y-[32px]">
           <ProfileInfo label="이름" value="김진원" />
@@ -20,9 +26,24 @@ const AdminProfile = () => {
           <ProfileInfo label="이메일" value="jin12345@gmail.com" />
         </div>
       </div>
-      <label onClick={() => logout()}>
-        <Logout />
-      </label>
+      <div>
+        <button onClick={handleLogoutClick}>
+          <Logout />
+        </button>
+        {isToggleLogout && (
+          <div className="absolute right-0 top-[30px] flex h-fit flex-col gap-2 rounded-[6px] border border-gray-200 bg-white p-2 shadow-[0px_4px_4px_0px_rgba(0,_0,_0,_0.25)]">
+            <button
+              onClick={() => logout()}
+              className="w-full rounded-[6px] px-5 py-2 text-body2 text-gray-500 hover:bg-error hover:text-white"
+            >
+              로그아웃
+            </button>
+            <button className="w-full rounded-[6px] px-5 py-2 text-body2 text-gray-500 hover:bg-error hover:text-white">
+              회원탈퇴
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
