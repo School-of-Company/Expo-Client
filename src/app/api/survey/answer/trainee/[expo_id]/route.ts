682100989b383e184d9c20dde9f80752f33a4360
Ethen1264 <jxorhks86909763@gmail.com>
2025-03-01 00:39:24 +0900

    ✨Add: set route ts

---
+++ src/app/api/survey/answer/trainee/[expo_id]/route.ts
@@ -0,0 +1,36 @@
+import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { apiClient } from '@/shared/libs/apiClient';
+
+export async function POST(
+  request: Request,
+  { params }: { params: { expo_id: number } },
+) {
+  const body = await request.json();
+  const { expo_id } = params;
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  const config = accessToken
+    ? {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    : {};
+  try {
+    const response = await apiClient.post(
+      `survey/answer/trainee/${expo_id}`,
+      body,
+      config,
+    );
+    return NextResponse.json(response.data, { status: response.status });
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message: string }>;
+
+    const status = axiosError.response?.status;
+    const message = axiosError.response?.data?.message;
+
+    return NextResponse.json({ error: message }, { status });
+  }
+}
