b7eb56bd9b621854193330a682f40b38bcc0a9a3
Ethen1264 <jxorhks86909763@gmail.com>
2025-03-10 14:11:39 +0900

    💚 fix: ci cd

--- src/widgets/signUp/model/useSignup.ts
+++ src/widgets/signUp/model/useSignup.ts
@@ -1,15 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
+import { SignUpData } from '@/shared/types/signup/type';
 import { postSignup } from '../api/postSignup';
-
-interface SignUpData {
-  name: string;
-  nickname: string;
-  email: string;
-  password: string;
-  phoneNumber: string;
-}
 
 export const useSignup = () => {
   const router = useRouter();
