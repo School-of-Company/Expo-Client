4b31aa6cd8af40c071cd0e82249eef9bce7311f1
bae080311 <s24024@gsm.hs.kr>
2025-03-27 00:00:11 +0900

    update: 제목목 왼쪽 정렬

--- src/widgets/expo-detail/ui/ExpoDetailLayout/index.tsx
+++ src/widgets/expo-detail/ui/ExpoDetailLayout/index.tsx
@@ -24,33 +24,31 @@ const ExpoDetailLayout = ({
   const date = `${expoDetail?.startedDay} ~ ${expoDetail?.finishedDay}`;
 
   return (
-    <div>
+    <div className="ml-[20px] mt-[48px] flex flex-col space-y-9">
       <DetailHeader headerTitle={expoDetail?.title} />
-      <div className="ml-[20px] mt-[48px] flex space-y-9">
-        <div className="space-y-[36px]">
-          <Image
-            src={expoDetail?.coverImage}
-            alt="TestClubImg"
-            objectFit="cover"
-            className="rounded-md"
-            width={752}
-            height={360}
-          />
-          <ContentText title="소개 글" content={expoDetail?.description} />
-          <ContentText title="모집 기간" content={date} />
-          {expoStandard?.length > 0 && (
-            <ContentListText data={expoStandard} title="참여자 연수" />
-          )}
-          {expoTraining?.essential.length > 0 && (
-            <ContentListText data={expoTraining?.essential} title="필수 연수" />
-          )}
-          {expoTraining?.choice.length > 0 && (
-            <ContentListText data={expoTraining.choice} title="선택 연수" />
-          )}
-          <div className="space-y-4">
-            <ContentText title="장소 지도" content={expoDetail?.location} />
-            <KakaoMap latitude={expoDetail?.x} longitude={expoDetail?.y} />
-          </div>
+      <div className="space-y-[36px]">
+        <Image
+          src={expoDetail?.coverImage}
+          alt="TestClubImg"
+          objectFit="cover"
+          className="rounded-md"
+          width={752}
+          height={360}
+        />
+        <ContentText title="소개 글" content={expoDetail?.description} />
+        <ContentText title="모집 기간" content={date} />
+        {expoStandard?.length > 0 && (
+          <ContentListText data={expoStandard} title="참여자 연수" />
+        )}
+        {expoTraining?.essential.length > 0 && (
+          <ContentListText data={expoTraining?.essential} title="필수 연수" />
+        )}
+        {expoTraining?.choice.length > 0 && (
+          <ContentListText data={expoTraining.choice} title="선택 연수" />
+        )}
+        <div className="space-y-4">
+          <ContentText title="장소 지도" content={expoDetail?.location} />
+          <KakaoMap latitude={expoDetail?.x} longitude={expoDetail?.y} />
         </div>
       </div>
     </div>
