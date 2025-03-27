31baa737764d657006a53c53e429b4ec3b31f8c5
bae080311 <s24024@gsm.hs.kr>
2025-03-27 13:28:19 +0900

    Feat: title content 사이 gap

--- src/entities/expo-detail/ui/ContentText/index.tsx
+++ src/entities/expo-detail/ui/ContentText/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ContentText = ({ title, content }: Props) => {
   return (
-    <div className="space-y-4">
+    <div className="flex flex-col gap-[0.88rem] space-y-4">
       <p className="text-body1b text-gray-600">{title}</p>
       <p className="text-body2 text-gray-400">{content}</p>
     </div>
