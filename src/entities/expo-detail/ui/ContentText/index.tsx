fbbcf51b9e5cecbfa407982971f9930bc929d0e1
bae080311 <s24024@gsm.hs.kr>
2025-03-28 16:08:14 +0900

    feat: 더보기 기능능

--- src/entities/expo-detail/ui/ContentText/index.tsx
+++ src/entities/expo-detail/ui/ContentText/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 interface Props {
   title: string;
@@ -6,10 +6,27 @@ interface Props {
 }
 
 const ContentText = ({ title, content }: Props) => {
+  const [show, setShow] = useState(false);
+  const ContentRef = useRef<HTMLDivElement>(null);
   return (
-    <div className="flex flex-col gap-[0.88rem] space-y-4">
+    <div className="flex flex-col gap-[0.88rem] space-y-4 overflow-hidden">
       <p className="text-body1b text-gray-600">{title}</p>
-      <p className="text-body2 text-gray-400">{content}</p>
+      <div>
+        <p
+          ref={ContentRef}
+          className={`text-body2 ${!show && 'line-clamp-5'} overflow-hidden break-words text-gray-400`}
+        >
+          {content}
+        </p>
+        {(ContentRef.current?.scrollHeight ?? 0) > 120 && (
+          <button
+            className={`text-left text-caption1r ${show ? 'text-main-600' : 'text-gray-300'}`}
+            onClick={() => setShow(!show)}
+          >
+            {show ? '접기' : '더보기'}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
