dcdfa48ebe8842b83bda1c27b2d5ba68156cdf47
Ethen1264 <jxorhks86909763@gmail.com>
2025-04-09 17:14:47 +0900

    update: middleware설정 변경

--- src/views/application/ui/application/index.tsx
+++ src/views/application/ui/application/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ApplicationLayout from '@/widgets/application/ui/ApplicationLayout';
-import { Header } from '@/widgets/layout';
 
 const Application = ({ params }: { params: string }) => {
   return (
-    <div className="flex h-screen flex-col mobile:gap-0">
-      <Header />
+    <div className="minh-screen flex flex-col mobile:gap-0">
       <div className="flex justify-center gap-[20px] px-5 py-[30px] mobile:flex-col">
         <div className="w-full max-w-[842px]">
           <ApplicationLayout params={params} />
