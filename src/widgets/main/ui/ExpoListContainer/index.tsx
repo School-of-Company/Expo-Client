8f8e4bcbad2a7261aa48d4deddd2382282e0ced2
Ethen1264 <jxorhks86909763@gmail.com>
2025-04-14 09:18:35 +0900

    feat: 박람회 없을때 컴포넌트 제작

--- src/widgets/main/ui/ExpoListContainer/index.tsx
+++ src/widgets/main/ui/ExpoListContainer/index.tsx
@@ -8,13 +8,14 @@ import { ExpoItem } from '@/shared/types/main/type';
 import SortFilter from '@/shared/ui/SortFilter';
 import { filterOptions } from '../../constant/filterOptions';
 import { useExpoList } from '../../model/useExpoList';
+import EmptyExpoList from '../EmptyExpoList';
 
 const ExpoListContainer = () => {
   const { data: expoList, isLoading } = useExpoList();
   const [selectedFilter, setSelectedFilter] = useState(filterOptions[0]);
 
   const sortedExpoList = useMemo<ExpoItem[]>(
-    () => sortedData(expoList, selectedFilter.value),
+    () => sortedData(expoList ?? [], selectedFilter.value),
     [expoList, selectedFilter.value],
   );
 
@@ -22,28 +23,34 @@ const ExpoListContainer = () => {
     isLoading,
     children: (
       <div className="flex w-full max-w-[1200px] flex-1 flex-col overflow-auto">
-        <div className="mb-[30px] flex justify-between">
-          <p className="text-h1m text-black">박람회 신청</p>
-          <SortFilter
-            options={filterOptions}
-            selectedOption={selectedFilter}
-            setSelectedOption={setSelectedFilter}
-          />
-        </div>
+        {sortedExpoList.length > 0 ? (
+          <>
+            <div className="mb-[30px] flex justify-between">
+              <p className="text-h1m text-black">박람회 신청</p>
+              <SortFilter
+                options={filterOptions}
+                selectedOption={selectedFilter}
+                setSelectedOption={setSelectedFilter}
+              />
+            </div>
 
-        <div className="grid grid-cols-3 gap-x-36 gap-y-24 mobile:grid-cols-1">
-          {sortedExpoList?.map((item, index) => (
-            <ExpoListItem
-              id={item.id}
-              coverImage={item.coverImage}
-              key={index}
-              title={item.title}
-              description={item.description}
-              startedDay={item.startedDay}
-              finishedDay={item.finishedDay}
-            />
-          ))}
-        </div>
+            <div className="grid grid-cols-3 gap-x-36 gap-y-24 mobile:grid-cols-1">
+              {sortedExpoList.map((item, index) => (
+                <ExpoListItem
+                  id={item.id}
+                  coverImage={item.coverImage}
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  startedDay={item.startedDay}
+                  finishedDay={item.finishedDay}
+                />
+              ))}
+            </div>
+          </>
+        ) : (
+          <EmptyExpoList />
+        )}
       </div>
     ),
   });
