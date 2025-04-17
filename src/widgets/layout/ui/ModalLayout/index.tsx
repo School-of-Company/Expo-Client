eaf05ad44202fdfe5b23153687ac6b3ca6bf5ae2
Ethen1264 <jxorhks86909763@gmail.com>
2025-04-17 23:40:33 +0900

    feat: 모달이 열리면 스크롤이 안되게 방지

--- src/widgets/layout/ui/ModalLayout/index.tsx
+++ src/widgets/layout/ui/ModalLayout/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const ModalLayout = ({ children }: Props) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
   return (
     <div
       className="fixed inset-0 z-10 flex items-center justify-center px-[18px]"
