c2fa37eaf58ea58722868924ece0501816c8d4c2
Ethen1264 <jxorhks86909763@gmail.com>
2025-04-21 15:16:28 +0900

    update: qr 스캔 로직 변경

--- src/widgets/name-tag/ui/NameTagForm/index.tsx
+++ src/widgets/name-tag/ui/NameTagForm/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { toast } from 'react-toastify';
 import NameTagHeader from '@/entities/name-tag/ui/NameTagHeader';
 import { useQRScanner } from '@/shared/model/useQRScanner';
 import { QrScanData } from '@/shared/types/common/QrScanData';
@@ -22,36 +21,25 @@ const NameTagForm = ({ id }: { id: string }) => {
   const [scannedQR, setScannedQR] = useState<QrScanData | null>(null);
   const [userData, setUserData] = useState<AttendUserResponse[]>([]);
   const { mutateAsync: attendUser } = usePatchAttendUserMutation(id);
+
   useQRScanner(setScannedQR);
 
   const fetchUserData = async (scannedQR: QrScanData) => {
     const authority = scannedQR.traineeId ? 'ROLE_TRAINEE' : 'ROLE_STANDARD';
-
-    const alreadyExists = userData.some(
-      (user) => user.phoneNumber === scannedQR.phoneNumber,
-    );
-    if (alreadyExists) {
-      toast.info('이미 스캔된 사용자입니다.');
-      return;
-    }
-
     const newUser: AttendUserResponse = await attendUser({
       authority,
       phoneNumber: scannedQR.phoneNumber,
     });
-
     setUserData((prev) => [...prev, newUser]);
   };
 
   useEffect(() => {
-    if (scannedQR) {
-      const handleScan = async () => {
-        await fetchUserData(scannedQR);
-        setScannedQR(null);
-      };
-
-      handleScan();
-    }
+    if (!scannedQR) return;
+    const handleScan = async () => {
+      await fetchUserData(scannedQR);
+      setScannedQR(null);
+    };
+    handleScan();
   }, [scannedQR]);
 
   const userQrPrintActions = {
@@ -61,6 +49,7 @@ const NameTagForm = ({ id }: { id: string }) => {
   return (
     <div className="flex w-full max-w-[1200px] flex-1 flex-col space-y-30 overflow-y-auto">
       <NameTagHeader />
+
       <TableForm
         categories={requestPrintCategories}
         data={userData}
