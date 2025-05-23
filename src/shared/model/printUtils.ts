da3b587f58eb1fd524ad318816deaece5e57aaae
Ethen1264 <jxorhks86909763@gmail.com>
2025-05-23 18:25:01 +0900

    hotfix: qr 사이즈 키움

--- src/shared/model/printUtils.ts
+++ src/shared/model/printUtils.ts
@@ -76,8 +76,8 @@ export const printBadge = (selectedData: {
                   const decodedQRText = decodeURIComponent("${encodedQRCode}");
                   new QRCode("qrcode", {
                     text: decodedQRText,
-                    width: 100,
-                    height: 100
+                    width: 150,
+                    height: 150
                   });
                 `
                 : ''
