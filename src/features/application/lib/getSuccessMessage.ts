babe72c581df099534142a6fe2894c2be73e71d2
Ethen1264 <jxorhks86909763@gmail.com>
2025-07-14 10:03:48 +0900

    hotfix: header naming

--- src/features/application/lib/getSuccessMessage.ts
+++ src/features/application/lib/getSuccessMessage.ts
@@ -4,18 +4,18 @@ export const getSuccessMessage = (
 ): string => {
   if (userType === 'STANDARD') {
     if (formType === 'application')
-      return '2025 광주광역시교육청 AI•SW체험축전 등록이 완료되었습니다.';
+      return '제2회 Dev Fest 등록이 완료되었습니다.';
     if (formType === 'survey')
-      return '2025 광주광역시교육청 AI•SW체험축전 만족도 조사 등록이 완료되었습니다.';
-    return '2025 광주광역시교육청 AI•SW체험축전 등록이 완료되었습니다.';
+      return '제2회 Dev Fest 만족도 조사 등록이 완료되었습니다.';
+    return '제2회 Dev Fest 등록이 완료되었습니다.';
   }
 
   if (userType === 'TRAINEE') {
     if (formType === 'application')
-      return '연수자 박람회 등록이 완료되었습니다.';
+      return '제2회 Dev Fest 등록이 완료되었습니다.';
     if (formType === 'survey')
-      return '연수자 만족도 조사 제출이 완료되었습니다.';
-    return '연수자 박람회 등록이 완료되었습니다.';
+      return '제2회 Dev Fest 만족도 조사 등록이 완료되었습니다.';
+    return '제2회 Dev Fest 등록이 완료되었습니다.';
   }
 
   return '박람회 등록이 완료되었습니다';
