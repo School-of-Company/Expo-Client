306de3bb7e1bfdafa123ef960a6519468fe244e7
bae080311 <s24024@gsm.hs.kr>
2025-10-26 14:04:21 +0900

    feat: use client

--- src/features/exhibition/edit/model/useExpoData.ts
+++ src/features/exhibition/edit/model/useExpoData.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { useQuery } from '@tanstack/react-query';
 import { useExpoDetail } from '@/shared/queries/useExpoDetail';
 import { useStandardProgram } from '@/shared/queries/useStandardProgram';
