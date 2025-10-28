3096783698ec97c4a083585a5c48977907658895
재욱 <haensol976520@gmail.com>
2025-10-28 10:08:13 +0900

    fix: PRIVACYCONSENT 확장

--- src/entities/form/constants/selectOptionData.tsx
+++ src/entities/form/constants/selectOptionData.tsx
@@ -1,4 +1,5 @@
 import {
+  Check,
   CheckBoxIcon,
   DropDown,
   MultipleChoice,
@@ -11,4 +12,5 @@ export const selectOptionData: Option[] = [
   { value: 'CHECKBOX', label: '체크박스', icon: <CheckBoxIcon /> },
   { value: 'DROPDOWN', label: '드롭다운', icon: <DropDown /> },
   { value: 'MULTIPLE', label: '객관식', icon: <MultipleChoice /> },
+  { value: 'PRIVACYCONSENT', label: '개인정보 동의', icon: <Check /> },
 ];
