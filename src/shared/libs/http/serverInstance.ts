98c8ce313b54f584cac3aabaa97918345cccf0db
재욱 <haensol976520@gmail.com>
2025-10-29 09:49:58 +0900

    feat: createHttpsAgent

--- src/shared/libs/http/serverInstance.ts
+++ src/shared/libs/http/serverInstance.ts
@@ -1,10 +1,25 @@
+import fs from 'fs';
 import https from 'https';
+import path from 'path';
 import axios from 'axios';
+
+const createHttpsAgent = () => {
+  try {
+    const certPath = path.join(process.cwd(), 'certificate.crt');
+    const ca = fs.readFileSync(certPath);
+    return new https.Agent({
+      ca,
+      rejectUnauthorized: true,
+    });
+  } catch (error) {
+    return new https.Agent({
+      rejectUnauthorized: process.env.NODE_ENV === 'production',
+    });
+  }
+};
 
 export const serverInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: process.env.NODE_ENV === 'production',
-  }),
+  httpsAgent: createHttpsAgent(),
 });
