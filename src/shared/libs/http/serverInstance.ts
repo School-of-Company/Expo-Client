26b8f44fd172bf21ffc511a159c69e4daae5596f
재욱 <haensol976520@gmail.com>
2025-10-29 15:31:50 +0900

    fix: ssl 우회

--- src/shared/libs/http/serverInstance.ts
+++ src/shared/libs/http/serverInstance.ts
@@ -1,23 +1,10 @@
-import fs from 'fs';
 import https from 'https';
-import path from 'path';
 import axios from 'axios';
-
-const createHttpsAgent = () => {
-  try {
-    const certPath = path.join(process.cwd(), 'certificate.crt');
-    const ca = fs.readFileSync(certPath);
-    return new https.Agent({
-      ca,
-      rejectUnauthorized: true,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 export const serverInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
-  httpsAgent: createHttpsAgent(),
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
