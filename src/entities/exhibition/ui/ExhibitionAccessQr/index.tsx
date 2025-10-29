4be35021c2d8a66a24c65fcfbadc22bb47e59ec8
재욱 <haensol976520@gmail.com>
2025-10-29 16:56:34 +0900

    feat: qa 라우팅

--- src/entities/exhibition/ui/ExhibitionAccessQr/index.tsx
+++ src/entities/exhibition/ui/ExhibitionAccessQr/index.tsx
@@ -3,14 +3,16 @@ import { QRCodeSVG } from 'qrcode.react';
 const ExhibitionAccessQr = ({
   id,
   userType,
+  applicationType,
 }: {
   id: string;
   userType: string;
+  applicationType: string;
 }) => {
   return (
     <QRCodeSVG
       className="h-[420px] w-[420px] tablet:h-[280px] tablet:w-[280px]"
-      value={`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/application/${id}?formType=application&userType=${userType}&applicationType=onsite`}
+      value={`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/application/${id}?formType=application&userType=${userType}&applicationType=${applicationType}`}
     />
   );
 };
