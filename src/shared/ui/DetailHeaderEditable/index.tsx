e6d792d594c0cc74e2b93088b46423c5f315d19b
bae080311 <s24024@gsm.hs.kr>
2025-10-31 12:25:19 +0900

    feat: register props

--- src/shared/ui/DetailHeaderEditable/index.tsx
+++ src/shared/ui/DetailHeaderEditable/index.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 import { ArrowLeft } from '@/shared/assets/icons';
 import Input from '../Input';
 
 interface Props {
   textCenter?: boolean;
+  registration: UseFormRegisterReturn;
 }
 
-const DetailHeaderEditable = ({ textCenter = false }: Props) => {
+const DetailHeaderEditable = ({ textCenter = false, registration }: Props) => {
   const router = useRouter();
 
   return (
@@ -20,8 +22,10 @@ const DetailHeaderEditable = ({ textCenter = false }: Props) => {
       <Input
         placeholder="폼의 제목을 입력해주세요"
         className={`${textCenter ? 'text-center' : ''} flex-grow`}
-        name="title"
-        aria-hidden={false}
+        name={registration.name}
+        onBlur={registration.onBlur}
+        onChange={registration.onChange}
+        ref={(el) => registration.ref(el)}
       />
     </div>
   );
