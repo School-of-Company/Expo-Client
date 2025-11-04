308caf356f2b6fe9be24955af314f90f47ab1314
bae080311 <s24024@gsm.hs.kr>
2025-11-04 17:10:34 +0900

    fix: end point

--- src/features/exhibition/participants/api/getTraineeExcelFile.ts
+++ src/features/exhibition/participants/api/getTraineeExcelFile.ts
@@ -3,7 +3,7 @@ import clientTokenInstance from '@/shared/libs/http/clientTokenInstance';
 
 export const getTraineeExcelFile = async (id: string) => {
   try {
-    const response = await clientTokenInstance.get(`/trainee/class/${id}`, {
+    const response = await clientTokenInstance.get(`/trainee/${id}`, {
       headers: {
         'X-File-Download': 'true',
       },
