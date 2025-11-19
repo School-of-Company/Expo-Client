59d2ad011ce2723aec651440885a7c90c527a7d9
재욱 <haensol976520@gmail.com>
2025-11-19 20:45:21 +0900

    fix: informationJsonData에 FIELD일때만 name 제거

--- src/features/application/lib/formatter/createStandardApplicationFormatter.ts
+++ src/features/application/lib/formatter/createStandardApplicationFormatter.ts
@@ -4,25 +4,43 @@ import {
   DynamicFormValues,
   FormattedApplicationData,
 } from '@/shared/types/application/type';
+import { ApplicationType } from '@/shared/types/exhibition/type';
 import { processDynamicFormData } from '../process/processDynamicFormData';
 
 export const createStandardApplicationFormatter = (
   dynamicFormItems: DynamicFormItem[],
+  applicationType?: ApplicationType,
 ) => {
   return (
     data: DynamicFormValues & { privacyConsent: boolean },
   ): FormattedApplicationData => {
-    const nameField = dynamicFormItems.find((item) =>
-      item.title.includes('이름'),
-    );
-    const nameValue = nameField
-      ? (data[slugify(nameField.title)] as string | undefined)
-      : undefined;
+    const processedData = processDynamicFormData(data, dynamicFormItems);
+
+    let nameValue: string | undefined;
+    let informationJsonData = processedData;
+
+    if (applicationType === 'FIELD') {
+      const nameField = dynamicFormItems.find((item) =>
+        item.title.includes('성명'),
+      );
+
+      if (nameField) {
+        nameValue = data[slugify(nameField.title)] as string | undefined;
+
+        const { [nameField.title]: _, ...rest } = processedData;
+        informationJsonData = rest;
+      }
+    } else {
+      const nameField = dynamicFormItems.find((item) =>
+        item.title.includes('이름'),
+      );
+      nameValue = nameField
+        ? (data[slugify(nameField.title)] as string | undefined)
+        : undefined;
+    }
 
     return {
-      informationJson: JSON.stringify(
-        processDynamicFormData(data, dynamicFormItems),
-      ),
+      informationJson: JSON.stringify(informationJsonData),
       ...(data.privacyConsent !== undefined && {
         personalInformationStatus: data.privacyConsent,
       }),
